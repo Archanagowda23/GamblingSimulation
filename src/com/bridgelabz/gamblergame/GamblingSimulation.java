a08e598f2ee8a6f21ebe6bdaa30dd50b44216e04
Archanagowda23 <archanar249@gmail.com>
2022-03-11 13:30:31 +0530

    Added:UC1 Stake and Bet Initialized

--- src/com/bridgelabz/gamblergame/GamblingSimulation.java
+++ src/com/bridgelabz/gamblergame/GamblingSimulation.java
@@ -2,8 +2,13 @@ package com.bridgelabz.gamblergame;
 
 public class GamblingSimulation {
 
+    public static final int STARTING_DAILY_STAKE = 100;
+    public static final int BET = 1;
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to Gambler Simulator Program");
+        System.out.println("Starting Daily Stake : "+ STARTING_DAILY_STAKE);
+        System.out.println("Starting Daily Stake : "+ BET);
     }
 }
